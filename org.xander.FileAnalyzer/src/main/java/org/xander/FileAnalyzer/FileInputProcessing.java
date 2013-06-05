56bc4b6252c1220946bd40c4560d5c7816af6efd
ale <ale@ale.(none)>
2013-06-05 12:28:32 +0300

    removed old module

--- org.xander.FileAnalyzer/src/main/java/org/xander/FileAnalyzer/FileInputProcessing.java
+++
@@ -1,9 +0,0 @@
-package org.xander.FileAnalyzer;
-
-public class FileInputProcessing {
-    public static void main( String[] args ) {
-        String fileToOpen = args[0];
-        FileInternalProcessing fileInternalProcessing = new FileInternalProcessing(fileToOpen);
-        fileInternalProcessing.workWithFile();
-    }
-}
